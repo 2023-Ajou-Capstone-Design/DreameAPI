f639a859fe2e2d94a7fc1c76b3de4c9d5674f73d
Yegeurina <yegeurinasec@gmail.com>
2023-04-09 21:49:58 +0900

    Feat : 내가쓴글 리스트

--- app.py
+++ app.py
@@ -361,11 +361,38 @@ def FoodDetail():
 
 # # 로그인
 
-# # 마이페이지
-# ## 내가 쓴 글 리스트
-# @app.route("/MyPage/myList",methods = ["POST"])
-# def FoodshareMyList():
-#     pass
+# 마이페이지
+## 내가 쓴 글 리스트
+@app.route("/MyPage/myList",methods = ["POST"])
+def FoodshareMyList():
+    uID = request.values.get('UserID')
+    con = pymysql.connect(host='dreame.ceneilkum8gx.us-east-2.rds.amazonaws.com', 
+                            user='dreameAdmin', password='dreame9785',
+                            db='Dreame', charset='utf8')
+    cur = con.cursor()
+    sql = "SELECT * FROM FoodShare WHERE (UserID like %s)"
+    cur.execute(sql,( uID))
+    
+    rows = cur.fetchall()
+    keys = ("WritingID","UploadTime","Title","Contents","Photo1","Photo2","Photo3","UserID")
+    items = [dict(zip(keys,row)) for row in rows]
+    for item in items :
+        
+        item["WritingID"] = str(item["WritingID"])
+        item["UploadTime"] = str(item["UploadTime"])
+        item["Title"] = str(item["Title"])
+        item["Contents"] = str(item["Contents"])
+        item["Photo1"] = base64ToString(item["Photo1"])
+        item["Photo2"] = base64ToString(item["Photo2"])
+        item["Photo3"] = base64ToString(item["Photo3"])
+        item["UserID"] = str(item["UserID"])
+    con.close()
+    
+    return{
+        "total" : len(rows),
+        "items" : items
+    }
+    pass
 
 
 if __name__ == "__main__" :
