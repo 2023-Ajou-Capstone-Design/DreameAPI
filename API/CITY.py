81c92b1fda553e4d2a2f492f9228ddffc0c4e527
Yegeurina <yegeurinasec@gmail.com>
2023-05-19 23:45:06 +0900

    Feat : 군구리스트조회

--- API/CITY.py
+++ API/CITY.py
@@ -10,11 +10,46 @@ city_sql = sql.get("City")
 
 ##도 리스트 출력
 @city_bp.route("/Do",methods = ["POST"])
-def FoodshareMyList():
+def getDoList():
     sql = city_sql.get("Do")
     conn = DB()
     rows = conn.select(sql)
     keys = ("Do")
+    items = [dict(zip(keys,row)) for row in rows]
+    data = {
+        "total" : len(rows),
+        "items" : items
+    }
+    return jsonify(data)
+
+##시 리스트 출력
+@city_bp.route("/Si",methods = ["POST"])
+def getSiList():
+    Do = request.values.get('Do')
+    
+    sql = city_sql.get("Si")
+    conn = DB()
+    rows = conn.select(sql,(Do))
+    
+    keys = ("Si")
+    items = [dict(zip(keys,row)) for row in rows]
+    data = {
+        "total" : len(rows),
+        "items" : items
+    }
+    return jsonify(data)
+
+##시 리스트 출력
+@city_bp.route("/GunGu",methods = ["POST"])
+def getGunGuList():
+    Do = request.values.get('Do')
+    Si = request.values.get("Si")
+    
+    sql = city_sql.get("GunGu")
+    conn = DB()
+    rows = conn.select(sql,(Do,Si))
+    
+    keys = ("GunGu")
     items = [dict(zip(keys,row)) for row in rows]
     data = {
         "total" : len(rows),
